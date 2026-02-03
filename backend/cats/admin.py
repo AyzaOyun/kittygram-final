bf0f17d303888c473f9f76e34b0e1bf3b06abe2d
Айза <ayza.oyun@yandex.ru>
2026-02-03 23:35:38 +0300

    update workflows

--- backend/cats/admin.py
+++ backend/cats/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cat, Achievement, AchievementCat
 
-# Register your models here.
+admin.site.register(Cat)
+admin.site.register(Achievement)
+admin.site.register(AchievementCat)
+
