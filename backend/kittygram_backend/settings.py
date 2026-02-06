b811f0f579ffdd076e6e1d721db7ef192c58723f
Айза <ayza.oyun@yandex.ru>
2026-02-06 23:55:29 +0300

    red after the review4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -117,6 +117,6 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': int(os.getenv('PAGE_SIZE', 10)),
+    'PAGE_SIZE': 10,
 
 }
